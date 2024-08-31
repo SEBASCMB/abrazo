4c1ac7b7772ba6ec5de20d04d2d29f5c59d3263d
Sebastian Marquez <sebastiancmarquez1998@gmail.com>
2024-08-31 18:48:07 -0500

    Add: version 1.0

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function cambiaMensaje() {
+  document.getElementById("loading-message").innerText = "Solo para mi luisa🟣!";
+}
+
+setTimeout(cambiaMensaje, 2300);
+
+setTimeout(function () {
+  alert("Recibiste un abrazo a la lejania de parte de sebastian");
+}, 2400);
